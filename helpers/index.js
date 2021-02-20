c4500387820b8dc64aee363053ae7c78af8420bc
jurelik <jure@likar.me>
2021-02-20 17:25:20 +0000

    initDB set up with initial data

--- helpers/index.js
+++ helpers/index.js
@@ -4,16 +4,20 @@ const models = require('../models');
 
 const initDB = () => {
   models.sequelize.sync().then(async () => {
-    console.log('synced!')
-    //const t = await db.transaction();
+    const t = await db.transaction();
 
-    //try {
+    try {
+      await db.query(`INSERT INTO artists (name, bio, location,  "createdAt", "updatedAt") VALUES ('antik', 'hello world', 'earth', NOW(), NOW())`, { type: Sequelize.QueryTypes.INSERT, transaction: t });
+      await db.query(`INSERT INTO songs (title, "fileType", url, tags, "artistId", "createdAt", "updatedAt") VALUES ('test', 'mp3', '/ipfs/QmU1B9JdMvhm4EB8kj487GfwQzfVtocKCm9XNAHkUtHz4f', ARRAY ['lofi', 'hiphop'], 1, NOW(), NOW())`, { type: Sequelize.QueryTypes.INSERT, transaction: t });
+      await db.query(`INSERT INTO files (name, type, "fileType", url, tags, "songId", "createdAt", "updatedAt") VALUES ('snare', 'original', 'wav', '/ipfs/QmTp7eeKm1ymt6SZD3SPMD3mKkAFomE8x5xtJhqK48a8qy', ARRAY ['snare', 'lofi'], 1, NOW(), NOW())`, { type: Sequelize.QueryTypes.INSERT, transaction: t });
+      await db.query(`INSERT INTO comments (content, "artistId", "songId", "createdAt", "updatedAt") VALUES ('this is a comment', 1, 1, NOW(), NOW())`, { type: Sequelize.QueryTypes.INSERT, transaction: t });
 
-    //}
-    //catch (err) {
-    //  await t.rollback();
-    //  throw err;
-    //}
+      await t.commit();
+    }
+    catch (err) {
+      await t.rollback();
+      throw err;
+    }
   }).catch(err => {
     console.error(err);
   });
