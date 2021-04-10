6325ed7db8e63943c12d12bd25fa37887f9085a2
jurelik <jure@likar.me>
2021-04-10 11:17:07 +0100

    added touch and destroy functionality to sessionStore.js

--- sessionStore.js
+++ sessionStore.js
@@ -28,7 +28,7 @@ module.exports = function SessionStoreInit(Store) {
       const t = await this.db.transaction();
 
       try {
-        await this.db.query(`INSERT INTO sessions (sid, data, "createdAt", "updatedAt") VALUES ('${sid}', '${JSON.stringify(session)}', NOW(), NOW()) ON CONFLICT (sid) DO UPDATE SET data = '${JSON.stringify(session)}', "updatedAt" = NOW()`, { type: Sequelize.QueryTypes.SELECT, transaction: t });
+        await this.db.query(`INSERT INTO sessions (sid, data, "createdAt", "updatedAt") VALUES ('${sid}', '${JSON.stringify(session)}', NOW(), NOW()) ON CONFLICT (sid) DO UPDATE SET data = '${JSON.stringify(session)}', "updatedAt" = NOW()`, { type: Sequelize.QueryTypes.INSERT, transaction: t });
         await t.commit();
 
         return cb(null);
@@ -39,14 +39,34 @@ module.exports = function SessionStoreInit(Store) {
       }
     }
 
-    touch(sid, session, cb) {
-      console.log('touch');
-      cb();
+    async touch(sid, session, cb) {
+      const t = await this.db.transaction();
+
+      try {
+        await this.db.query(`UPDATE sessions SET "updatedAt" = NOW() WHERE sid = '${sid}'`, { type: Sequelize.QueryTypes.UPDATE, transaction: t });
+        await t.commit();
+
+        return cb(null);
+      }
+      catch (err) {
+        await t.rollback();
+        return cb(err);
+      }
     }
 
-    destroy(sid, cb) {
-      console.log('destroy');
-      cb();
+    async destroy(sid, cb) {
+      const t = await this.db.transaction();
+
+      try {
+        await this.db.query(`DELETE FROM sessions WHERE sid = '${sid}'`, { type: Sequelize.QueryTypes.DELETE, transaction: t });
+        await t.commit();
+
+        return cb(null);
+      }
+      catch (err) {
+        await t.rollback();
+        return cb(err);
+      }
     }
   }
 
