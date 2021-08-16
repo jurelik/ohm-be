a3e0503bd6a765954361481cf5e477727e0a591e
jurelik <jure@likar.me>
2021-08-16 21:09:34 +0100

    removed /api/ from routes as ohm-be will run on the api subdomain

--- index.js
+++ index.js
@@ -23,103 +23,103 @@ app.use(session({
 }))
 
 app.get('/', (req, res) => {
-  res.end('Hello world');
+  res.end('Hello traveler.');
 });
 
-app.post('/api/login', express.json(), (req, res) => {
+app.post('/login', express.json(), (req, res) => {
   helpers.postLogin(req, res);
 });
 
-app.post('/api/register', express.json(), (req, res) => {
+app.post('/register', express.json(), (req, res) => {
   helpers.postRegister(req, res);
 });
 
-app.post('/api/latest', express.json(), (req, res) => {
+app.post('/latest', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postLatest(req, res);
 });
 
-app.post('/api/feed', express.json(), (req, res) => {
+app.post('/feed', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postFeed(req, res);
 });
 
-app.get('/api/artist/:name', (req, res) => {
+app.get('/artist/:name', (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.getArtist(req, res);
 });
 
-app.get('/api/song/:id', (req, res) => {
+app.get('/song/:id', (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.getSongRoute(req, res);
 });
 
-app.get('/api/album/:id', (req, res) => {
+app.get('/album/:id', (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.getAlbumRoute(req, res);
 });
 
-app.get('/api/file/:id', (req, res) => {
+app.get('/file/:id', (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.getFile(req, res);
 });
 
-app.post('/api/upload', express.json(), (req, res) => {
+app.post('/upload', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postUpload(req, res);
 });
 
-app.post('/api/comment', express.json(), (req, res) => {
+app.post('/comment', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postComment(req, res);
 });
 
-app.post('/api/pinned', express.json(), (req, res) => {
+app.post('/pinned', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postPinned(req, res);
 });
 
-app.post('/api/search', express.json(), (req, res) => {
+app.post('/search', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postSearch(req, res);
 });
 
-app.post('/api/delete', express.json(), (req, res) => {
+app.post('/delete', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postDelete(req, res);
 });
 
-app.post('/api/following', express.json(), (req, res) => {
+app.post('/following', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postFollowing(req, res);
 });
 
-app.get('/api/follow/:id', express.json(), (req, res) => {
+app.get('/follow/:id', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.getFollow(req, res);
 });
 
-app.get('/api/unfollow/:id', express.json(), (req, res) => {
+app.get('/unfollow/:id', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.getUnfollow(req, res);
 });
 
-app.post('/api/changepassword', express.json(), (req, res) => {
+app.post('/changepassword', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postChangePassword(req, res);
 });
 
-app.post('/api/changelocation', express.json(), (req, res) => {
+app.post('/changelocation', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postChangeLocation(req, res);
 });
 
-app.post('/api/changebio', express.json(), (req, res) => {
+app.post('/changebio', express.json(), (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.postChangeBio(req, res);
 });
 
-app.get('/api/logout', (req, res) => {
+app.get('/logout', (req, res) => {
   if (!helpers.userAuthenticated(req, res)) return;
   helpers.getLogout(req, res);
 });
