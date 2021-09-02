acac6069e0119f00783c2647dfe69c92437ba67d
jurelik <jure@likar.me>
2021-09-02 12:45:13 +0100

    added max_memory_restart to production.config

--- config/production.config.js
+++ config/production.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   apps : [{
     name: "ipfs-daemon",
-    script: "ipfs daemon --enable-gc"
+    script: "ipfs daemon --enable-gc",
+    max_memory_restart: "500M"
   },
   {
     name: "main",
     script: "node",
-    args: "index.js"
+    args: "index.js",
+    max_memory_restart: "500M"
   },
   {
     name: "cron",
